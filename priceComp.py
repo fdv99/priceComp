6876c582cf9584827b5481a50b727e365ba640cb
V6-Engineering <forrest@v6carolina.com>
2020-03-23 14:50:39 -0400

    home depot price check works

--- priceComp.py
+++ priceComp.py
@@ -4,3 +4,14 @@ https://www.youtube.com/watch?v=rajXpxvHiV0
 """
 
 web = webdriver.Chrome('support files/chromedriver.exe')
+
+myupc = '0857749002198'
+url_homedepot = 'https://homedepot.com/s/%2522'+myupc+'%2522?NCNI-5'
+web.get(url_homedepot)
+
+title_homedepot = web.find_element_by_class_name('product-details__title').text
+price_homedepot = web.find_element_by_class_name('price').text[:-2]+'.'+web.find_element_by_class_name('price').text[-2:]
+
+print(myupc)
+print(title_homedepot)
+print(price_homedepot)
